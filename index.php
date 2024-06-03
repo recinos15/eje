ff7fee0b4215965d4260b4da365c2104ab676285
recinos15 <164216616+recinos15@users.noreply.github.com>
2024-06-02 21:43:15 -0600

    Update index.php
    se cambio el codigo

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
+<?php
+// Definir la fecha y hora actual
+$fecha_actual = date("d/m/Y H:i:s");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deploy</title>
+    <title>PHP en Vercel</title>
 </head>
 <body>
-<header>
-    <h1>Bienvenido</h1>
-</header>
-<section>
-<?php
-$fecha_actual = date("d/m/Y H:i:s");
-echo "<p>La fecha y la hora actual: $fecha_actual</p>";
-?>
-</section> 
-<footer>
-    <?php echo date("Y"); ?> Desarrollado por Roberto
-</footer>
+    <header>
+        <h1>Bienvenido a mi página PHP en Vercel</h1>
+    </header>
+    <section>
+        <p>La fecha y la hora actual: <?php echo $fecha_actual; ?></p>
+    </section> 
+    <footer>
+        <?php echo date("Y"); ?> Desarrollado por Roberto
+    </footer>
 </body>
 </html>
